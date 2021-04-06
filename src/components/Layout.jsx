76c470bcb6b26d6c14f7d468489d49d6bb4cd8e3
blackBox <dzzirima@gmail.com>
2021-04-07 00:35:42 +0200

    added left  permanent drawer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,45 @@
+import { makeStyles,Drawer, Typography } from '@material-ui/core'
 import React from 'react'
 
-const Layout = ({children}) => {
-    return (
-        <div>
-            layout
-            {children}
 
+
+// constant for the drawer with
+const drawerWidth = 240;
+const useStyles = makeStyles({
+    page:{
+        background:"#f8ede3",
+        width:'100%',
+        height :'100vh'
+    },
+    drawer:{
+        width:drawerWidth
+
+    },
+    paperDrawer:{
+        width:drawerWidth
+    },
+    root:{
+        display:"flex"
+    }
+})
+
+const Layout = ({children}) => {
+    const classes = useStyles()
+    return (
+        <div className = {classes.root}>
+        <Drawer 
+            className = {classes.drawer}
+            //overiding one of the classes inside the drawer
+            classes = {{paper:classes.paperDrawer}}
+            variant = "permanent"
+            anchor = "left"
+        >
+            <Typography> Heloo</Typography>
+        </Drawer>
+
+        <div className ={classes.page}>
+            {children}
+        </div>
         </div>
     )
 }
