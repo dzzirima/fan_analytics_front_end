0d8ccefa43705ba0f5baea5f4d6923b40b752fc5
blackBox <dzzirima@gmail.com>
2021-04-07 13:37:53 +0200

    added drawer list of components

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,12 @@
 import { makeStyles,Drawer, Typography } from '@material-ui/core'
 import React from 'react'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
+import SubjectOutlined from '@material-ui/icons/SubjectOutlined';
+import {useHistory,useLocation } from 'react-router-dom'
 
 
 
@@ -20,11 +27,30 @@ const useStyles = makeStyles({
     },
     root:{
         display:"flex"
+    },
+    active:{
+        background:'#d8ac9c'
     }
 })
+// menu itmes that are gonn be displayed on the drawer
+const menuItems = [
+    {
+        text:'My Notes',
+        icon:<SubjectOutlined/>,
+        path:'/'
+    },
+    {
+        text:'Create Notes',
+        icon:<AddCircleOutlinedIcon/>,
+        path:'/create'
+    },
+    
+]
 
 const Layout = ({children}) => {
     const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
     return (
         <div className = {classes.root}>
         <Drawer 
@@ -34,9 +60,29 @@ const Layout = ({children}) => {
             variant = "permanent"
             anchor = "left"
         >
-            <Typography> Heloo</Typography>
-        </Drawer>
+            <dv>
+             <Typography> Heloo</Typography>
+            </dv>
 
+            {/* list all the links */}
+            <List>
+                {
+                    menuItems.map((item) =>(
+                        <ListItem
+                            button
+                            key = {item.text}
+                            onClick= {() =>{history.push(item.path)}}
+                            className = {location.pathname == item.path?classes.active :null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary = {item.text}/>
+                        </ListItem>
+                    ))
+                }
+            </List>
+           
+            
+        </Drawer>
         <div className ={classes.page}>
             {children}
         </div>
