7279f68ce915fa21a0e6a9a164398a2f9b8d6c1b
David Zirima <davidtzirima@gmail.com>
2021-05-25 17:19:59 +0200

    working frontend code

--- src/pages/LocationReport.js
+++ src/pages/LocationReport.js
@@ -8,6 +8,7 @@ import { TextField } from '@material-ui/core';
 import {useHistory } from 'react-router-dom'
 import DataTable from '../components/DataTable';
 import api from '../api/api'
+import axios from '../api/Axios'
 
 const useStyles = makeStyles({
   btn:{
@@ -34,28 +35,34 @@ const theme = createMuiTheme({
 
 })
 export default function LocationReport(props) {
+  const [trackers, setTrackers] = useState([])
 
-async function  getTrackers(){
-  let trackers = await  api.getTrackers(JSON.parse(api.getGlobalHashFromLocalStorage()).hash)
-  return trackers
-}
-
-const history  = useHistory() 
-const classes = useStyles()
-const [trackers, setTrackers] = useState([])
-
-useEffect(async() => {
-  let trackersList = await  api.getTrackers(JSON.parse(api.getGlobalHashFromLocalStorage()).hash)
-  setTrackers(trackersList)
-  console.log(trackers)
-
-}, [])
+  useEffect(() => {
+    async function fetchData(){
+      let hash = await  api.getGlobalHashFromLocalStorage()
+      const result = await axios.get(`location?globalHash=${hash}`)
+      console.log(result)
+      setTrackers(result.data);
+      return result
+    }
+    fetchData()
+  }, [])
+  
 
   return (
-    
     <Container>
+
+      <div>
+        {
+          trackers.map((item) =>(
+            <h1> {item.name} </h1>
+          ))
+        }
+
+      </div>
+     
       <h1> Helllo Location Reports </h1>
-      <DataTable/>
+      {/* <DataTable/> */}
     </Container>
     
   )
