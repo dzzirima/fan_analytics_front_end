9b99fff047ef78558ac810bdf94c260e94b41548
dzzirima@gmail.com <dzzirima@gmail.com>
2021-06-10 09:38:22 +0200

    added digital ocean url

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
+let baseurl = 'https://fananalytics.herokuapp.com'
+let baseurl_digital_ocean = 'https://fanset-analytics-diygc.ondigitalocean.app/'
 
 const instance = axios.create({
     // baseURL: 'http://localhost:8001',
-    baseURL:'https://fananalytics.herokuapp.com'
+    baseURL:baseurl_digital_ocean
     
   });
 
