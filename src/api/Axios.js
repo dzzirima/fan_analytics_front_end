8de0a10ee22bce05e61dc97f29da77f2d84f9856
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2023-04-03 15:24:44 +0200

    feat : adding headers to the axios requests

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -3,8 +3,8 @@ let baseurl = 'https://fananalytics.herokuapp.com'
 let baseurl_digital_ocean = 'https://fanset-analytics-diygc.ondigitalocean.app/'
 
 const instance = axios.create({
-     baseURL: 'http://localhost:4000',
-    // baseURL:baseurl_digital_ocean,
+    // baseURL: 'http://localhost:4000',
+   baseURL:baseurl_digital_ocean,
     headers: {"app-client": "megamarket"}
     
   });
