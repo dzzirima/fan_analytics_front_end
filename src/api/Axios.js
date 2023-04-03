4e1e80c62c06d20770b0ab16f4aa4847e52e1e48
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2023-04-03 15:51:10 +0200

    fianl code

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -3,9 +3,9 @@ let baseurl = 'https://fananalytics.herokuapp.com'
 let baseurl_digital_ocean = 'https://fanset-analytics-diygc.ondigitalocean.app/'
 
 const instance = axios.create({
-    // baseURL: 'http://localhost:4000',
+     //baseURL: 'http://localhost:4000',
     baseURL:baseurl_digital_ocean,
-    headers: {"app-client": "megamarket"}
+    headers: {"app-client":"megamarket"}
     
   });
 
